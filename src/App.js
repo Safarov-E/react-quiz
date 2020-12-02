6df435a35e45cb84afbbbabf07fd5f43048a76d9
Safarov <elkhan.safarov10@yandex.com>
2020-12-02 14:59:45 +0500

    Создание главной страницы

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
+import Quiz from './containers/Quiz'
 import Layout from './hoc/Layout'
 
 class App extends Component {
   render() {
     return (
       <Layout>
-
-        <div style={{width: '400px', border: '1px solid black'}}>
-          <h1>Layout works</h1>
-        </div>
+        <Quiz />
       </Layout>
     )
   }
