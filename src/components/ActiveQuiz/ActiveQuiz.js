ca86e29100fddd4e4515a4051966b4e2895affce
Safarov <elkhan.safarov10@yandex.com>
2020-12-02 19:58:28 +0500

    Список вопросов

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import AnswersList from './AnswersList'
 import classes from './ActiveQuiz.module.css'
 
 const ActiveQuiz = props => (
@@ -10,12 +11,9 @@ const ActiveQuiz = props => (
             </span>
             <small>4 из 12</small>
         </p>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-        </ul>
+        <AnswersList
+            answers={props.answers}
+        />
     </div>
 )
 
