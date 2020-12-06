40b7788930e07ac173724a44db0c0e99bb6ffbda
Safarov <elkhan.safarov10@yandex.com>
2020-12-06 15:29:39 +0500

    Отображение состояния ответа

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -5,6 +5,7 @@ import classes from './Quiz.module.css'
 class Quiz extends Component {
   state = {
     activeQuestion: 0,
+    answerState: null,
     quiz: [
       {
         question: 'Какого цвета небо?',
@@ -31,10 +32,30 @@ class Quiz extends Component {
     ]
   }
   onAnswerClickHandler = (answerId) => {
-    console.log(answerId)
-    this.setState({
-      activeQuestion: this.state.activeQuestion + 1
-    })
+    const question = this.state.quiz[this.state.activeQuestion]
+    if(question.rightAnswerId === answerId) {
+      this.setState({
+        answerState: {[answerId]: 'success'}
+      })
+      const timeout = window.setTimeout(() => {
+        if(this.isQuizFinished()) {
+          
+        } else {
+          this.setState({
+            activeQuestion: this.state.activeQuestion + 1,
+            answerState: null
+          })
+        }
+        window.clearTimeout(timeout)
+      }, 1000)
+    } else {
+      this.setState({
+        answerState: {[answerId]: 'error'}
+      })
+    }
+  }
+  isQuizFinished = () => {
+    return this.state.activeQuestion + 1 === this.state.quiz.length
   }
   render() {
     return (
@@ -48,6 +69,7 @@ class Quiz extends Component {
                 onAnswerClick={this.onAnswerClickHandler}
                 quizLength={this.state.quiz.length}
                 answerNumber={this.state.activeQuestion + 1}
+                state={this.state.answerState}
               />
             </div>
         </div>
