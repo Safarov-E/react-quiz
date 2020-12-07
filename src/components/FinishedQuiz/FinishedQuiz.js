b0f4d16a646cde08597f6d5d8e01523f4c889aef
Safarov <elkhan.safarov10@yandex.com>
2020-12-07 15:37:58 +0500

    Вычисление результатов

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -11,24 +11,22 @@ const FinishedQuiz = props => {
     return (
         <div className={classes.FinishedQuiz}>
             <ul>
-                {
-                    props.quiz.map((quizItem, index) => {
-                        const cls = [
-                            'fa',
-                            props.results[quizItem.id] === 'error' ? 'fa-times' : 'fa-check',
-                            classes[props.results[quizItem.id]]
-                        ]
-                        return (
-                            <li
-                                key={index}
-                            >
-                                <strong>{index + 1}. </strong>
-                                {quizItem.question}
-                                <i className={cls.join(' ')} />
-                            </li>
-                        )
-                    })
-                }
+                {props.quiz.map((quizItem, index) => {
+                    const cls = [
+                        'fa',
+                        props.results[quizItem.id] === 'error' ? 'fa-times' : 'fa-check',
+                        classes[props.results[quizItem.id]]
+                    ]
+                    return (
+                        <li
+                            key={index}
+                        >
+                            <strong>{index + 1}. </strong>
+                            {quizItem.question}
+                            <i className={cls.join(' ')} />
+                        </li>
+                    )
+                })}
             </ul>
             <p>Правильно {successCount} из {props.quiz.length}</p>
             <div>
