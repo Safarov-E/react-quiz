9a3e882e1ab303533a93f65021d40178945f2a48
Safarov <elkhan.safarov10@yandex.com>
2020-12-16 21:02:46 +0500

    Создание страниц

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
-import Quiz from './containers/Quiz'
 import Layout from './hoc/Layout'
+import {Route, Switch} from 'react-router-dom'
+import Quiz from './containers/Quiz'
+import QuizList from './containers/QuizList'
+import Auth from './containers/Auth'
+import QuizCreator from './containers/QuizCreator'
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <Quiz />
+        <Switch>
+          <Route path='/auth' component={Auth} />
+          <Route path='/quiz-creator' component={QuizCreator} />
+          <Route path='/quiz/:id' component={Quiz} />
+          <Route path='/' component={QuizList} />
+        </Switch>
       </Layout>
     )
   }
