acb39484a2756d834860dd18cf61dcf41f681ff8
Safarov <elkhan.safarov10@yandex.com>
2020-12-21 14:58:12 +0500

    Валидация

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -4,6 +4,34 @@ import Button from '../../components/UI/Button'
 import Input from '../../components/UI/Input'
 
 export default class Auth extends Component {
+  state = {
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMessage: 'Введите корректный email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true
+        }
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Пароль',
+        errorMessage: 'Введите корректный пароль',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6
+        }
+      }
+    }
+  }
   loginHandler = () => {
 
   }
@@ -13,18 +41,39 @@ export default class Auth extends Component {
   submitHandler = event => {
     event.preventDefault()
   }
+  onChangeHandler = (event, controlName) => {
+    console.log(`${controlName}: `, event.target.value)
+  }
+  renderInputs = () => {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName]
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          shouldValidate={!!control.validation}
+          errorMessage={control.errorMessage}
+          onChange={event => this.onChangeHandler(event, controlName)}
+        />
+      )
+    })
+  }
   render() {
     return (
       <div className={classes.Auth}>
         <div>
           <h1>Авторизация</h1>
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+            { this.renderInputs() }
             <Input 
               label="Email" 
             />
             <Input 
               label="Пароль"
-              errorMessage={'Test'}
             />
             <Button 
               type="success" 
