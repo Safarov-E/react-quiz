29e34720015d10dd5f0e66531228fbf69ad1f498
Safarov <elkhan.safarov10@yandex.com>
2020-12-22 14:30:21 +0500

    Валидация формы

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,6 +5,7 @@ import Input from '../../components/UI/Input'
 import is from 'is_js'
 export default class Auth extends Component {
   state = {
+    isFormValid: false,
     formControls: {
       email: {
         value: '',
@@ -67,8 +68,15 @@ export default class Auth extends Component {
 
     formControls[controlName] = control
 
+    let isFormValid = true
+
+    Object.keys(formControls).forEach(name => {
+      isFormValid = formControls[name].valid && isFormValid
+    })
+
     this.setState({
-      formControls
+      formControls,
+      isFormValid
     })
   }
   renderInputs = () => {
@@ -99,12 +107,14 @@ export default class Auth extends Component {
             <Button 
               type="success" 
               onClick={this.loginHandler}
+              disabled={!this.state.isFormValid}
             >
               Войти
             </Button>
             <Button 
               type="primary" 
               onClick={this.registerHandler}
+              disabled={!this.state.isFormValid}
             >
               Зарегистрироваться
             </Button>
