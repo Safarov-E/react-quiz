47c644b2e81e5d57b9f5bbc1e90c527121e18aef
Safarov <elkhan.safarov10@yandex.com>
2020-12-24 17:55:12 +0500

    Изменение контролов

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -3,7 +3,7 @@ import classes from './QuizCreator.module.css'
 import Button from '../../components/UI/Button'
 import Input from '../../components/UI/Input'
 import Select from '../../components/UI/Select'
-import {createControl} from '../../form/formFramework'
+import {createControl, validate, validateForm} from '../../form/formFramework'
 import Auxiliary from '../../hoc/Auxiliary'
 
 function createOptionControl(number) {
@@ -30,20 +30,33 @@ function createFormControls() {
 export default class QuizCreator extends Component {
   state = {
     quiz: [],
+    isFormValid: false,
     rightAnswerId: 1,
     formControls: createFormControls()
   }
   submitHandler = (event) => {
     event.preventDefault()
   }
-  addQuestionHandler = () => {
-
+  addQuestionHandler = (event) => {
+    event.preventDefault()
   }
   createQuizHandler = () => {
 
   }
   changeHandler = (value, controlName) => {
+    const formControls = {...this.state.formControls}
+    const control = {...formControls[controlName]}
 
+    control.touched = true
+    control.value = value
+    control.valid = validate(control.value, control.validation)
+
+    formControls[controlName] = control
+
+    this.setState({
+      formControls,
+      isFormValid: validateForm(formControls)
+    })
   }
   renderControls = () => {
     return Object.keys(this.state.formControls).map((controlName, index) => {
@@ -95,12 +108,14 @@ export default class QuizCreator extends Component {
               <Button
                 type="primary"
                 onClick={this.addQuestionHandler}
+                disabled={!this.state.isFormValid}
               >
                 Добавить вопрос
               </Button>
               <Button
                 type="success"
                 onClick={this.createQuizHandler}
+                disabled={this.state.quiz.length === 0}
               >
                 Создать тест
               </Button>
