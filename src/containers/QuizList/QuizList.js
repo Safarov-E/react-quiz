4c286084586dd29824425c9f18eef0d30549600f
Safarov-E <elkhan.safarov10@yandex.com>
2021-01-05 12:47:09 +0500

    Компонент прогресса загрузки

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-import { NavLink } from 'react-router-dom'
 import classes from './QuizList.module.css'
+import { NavLink } from 'react-router-dom'
+import Loader from '../../components/UI/Loader'
 import axios from 'axios'
 
 export default class QuizList extends Component {
   state = {
-    quizes: []
+    quizes: [],
+    loading: true
   }
   renderQuizes = () => {
     return this.state.quizes.map((quiz, index) => {
@@ -31,7 +33,7 @@ export default class QuizList extends Component {
         })
       })
       this.setState({
-        quizes
+        quizes, loading: false
       })
     } catch(e) {
       console.log(e)
@@ -42,10 +44,13 @@ export default class QuizList extends Component {
         <div className={classes.QuizList}>
           <div>
             <h1>Список тестов</h1>
-
-            <ul>
-              {this.renderQuizes()}
-            </ul>
+            {
+              this.state.loading
+                ? <Loader />
+                : <ul>
+                    {this.renderQuizes()}
+                  </ul>
+            }
           </div>
         </div>
     )
